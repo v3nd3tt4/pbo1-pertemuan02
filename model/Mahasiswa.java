87170dfe33c713371f3cbf4b3903a489d58898a8
Okta Pilopa <pilopaokta@gmail.com>
2024-04-05 21:34:08 +0800

    Memanggil method dari banyak Classdi package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+	public static void tampilanInfo(){
+		System.out.println();
+		System.out.println("==Data Mahasiswa==============");
+		System.out.println("NPM: 2310010066");
+		System.out.println("Nama: Okta Pilopa");
+		System.out.println("==========================");
+	}
+}
