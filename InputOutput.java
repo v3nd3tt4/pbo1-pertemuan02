5508a4944e4316565827f2fb3b619395a883e6dc
Okta Pilopa <oktapilopa@gmail.com>
2024-04-05 21:56:55 +0800

    compile & run

--- InputOutput.java
+++ InputOutput.java
@@ -5,7 +5,7 @@ public class InputOutput{
 		Scanner scanner = new Scanner(System.in);
 		String nama;
 		System.out.println("Masukkan nama anda: ");
-		nama = scanner.nextline();
+		nama = scanner.nextLine();
 		System.out.println("Hai, "+nama);
 	}
 }
