b6aa58634a02970c1a2d163e762e94f08d8a9656
Okta Pilopa <oktapilopa@gmail.com>
2024-04-05 22:17:26 +0800

    add folder screenshot dan file gambar

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,20 @@ public class InputOutput{
 	public static void main(String [] args){
 		Scanner scanner = new Scanner(System.in);
 		String nama;
+		int jumlahSks;
+		Double IPK;		
+		
 		System.out.println("Masukkan nama anda: ");
 		nama = scanner.nextLine();
 		System.out.println("Hai, "+nama);
+		
+		System.out.println("Masukkan jumlah SKS yang sudah ditempuh: ");
+		jumlahSks = scanner.nextInt();
+		
+		System.out.println("Masukkan IPK sementara: ");
+		IPK = scanner.nextDouble();
+		
+		Double jumlah = IPK * jumlahSks;
+		System.out.println("Total Nilai = "+jumlah);
 	}
 }
